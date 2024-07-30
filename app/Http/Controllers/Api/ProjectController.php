e39d3a953fb6a5a341c0a42d06d9fa08264c6ef5
rinox3005 <peppe.rinox@gmail.com>
2024-07-30 17:57:31 +0200

    [UPD] handled pagination in api controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::with('technologies', 'type')->get();
+        $projects = Project::with('technologies', 'type')->paginate(2);
         return response()->json([
             'success' => true,
             'results' => $projects
